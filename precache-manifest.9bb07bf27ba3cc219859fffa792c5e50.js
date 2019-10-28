8e214b3971f69e6661d6faf0ccd51f9193e410b6
Muhammad Arif <arif1951995@gmail.com>
2019-10-28 11:30:41 -0700

    Updates

---
+++ precache-manifest.9bb07bf27ba3cc219859fffa792c5e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e84e2e361f1354c6da76318f42dd11",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "55ac257be62dc7495be1",
+    "url": "/burger-builder/static/css/main.f3647f2c.chunk.css"
+  },
+  {
+    "revision": "b723dc52ad98c04f1328",
+    "url": "/burger-builder/static/js/2.48c4779c.chunk.js"
+  },
+  {
+    "revision": "55ac257be62dc7495be1",
+    "url": "/burger-builder/static/js/main.17c8ad0c.chunk.js"
+  },
+  {
+    "revision": "01f389611d85dd339f28",
+    "url": "/burger-builder/static/js/runtime-main.fff5d8f9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
