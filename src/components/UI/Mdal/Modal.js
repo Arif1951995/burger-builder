691b2b106f1bffc334dabab7a9996f32ebeb4bd2
Muhammad Arif <arif1951995@gmail.com>
2019-10-28 13:05:29 -0700

    upadte modal.js

--- src/components/UI/Mdal/Modal.js
+++ src/components/UI/Mdal/Modal.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React  from 'react'
 import classes from './Modal.module.css'
 import HOC from '../../../Hoc/HOC'
 import Backdrop from '../Backdrop/Backdrop'
 
-function Modal(props) {
-    return (
-        <HOC>
-            <Backdrop 
-            show={props.show}
-            backdropCLicked={props.backdropCLicked}
-            />
-            <div className={classes.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translate(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}
-        >
-            {props.children}
-        </div>
 
-        </HOC>
-        
-    )
-}
+const Modal = React.memo(
+    
+    props => {
+        console.log('rendered')
+        return (
+            
 
-export default Modal
+            <HOC>
+                <Backdrop 
+                show={props.show}
+                backdropCLicked={props.backdropCLicked}
+                />
+                <div className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translate(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}
+            >
+                {props.children}
+            </div>
+    
+            </HOC>
+            
+        )
+
+    },
+    (prevProps, nextProps) => prevProps.show === nextProps.show
+  );
+
+export default React.memo(Modal)
